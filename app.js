7bf6740886dbbd4d2add467c2a63598d670d89b3
Benny Borunda <blznk128@yahoo.com>
2019-02-25 17:51:32 -0700

    added correct movements depending on right or wrong answer

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
+let int1 = Math.floor((Math.random() * 11) +1);
+let int2 = Math.floor((Math.random() * 11) +1);
+$('#question').text(int1 + ' ' + '+' + int2 + ' ' + '=')
+
+function generateNumbers() {
+     int1 = Math.floor((Math.random() * 11) +1);
+     int2 = Math.floor((Math.random() * 11) +1);
+     $('#question').text(int1 + ' ' + '+' + int2 + ' ' + '=')
+}
+
 let hi = () => {
-    let answer = $('#answer').val()
-    if (answer === '4') {
+    let answer = parseInt($('#answer').val());
+    console.log(int1 + int2)
+    if (answer === int1 + int2) {
         moveRight()
+        generateNumbers()
     } else {
         moveLeft()
     }
 }
 
-// 
+// sets the img
 var imgObj = null;
 function right() {
     imgObj = $('#ball')[0];
-    imgObj.style.position= 'absolute'; 
     imgObj.style.left = '700px';
-    imgObj.style.right = '700px';
 }
-
+// moves the image right or left depending on answer
 function moveRight () {
-    imgObj.style.left = parseInt(imgObj.style.left) + 30 + 'px';
-    console.log(imgObj.style.left)
+    imgObj.style.left = parseInt(imgObj.style.left) + 125 + 'px';
 }
-// 
-right()
 
 function moveLeft() {
-    imgObj.style.left = parseInt(imgObj.style.left) - 30 + 'px';
-    console.log(imgObj.style.right)
+    imgObj.style.left = parseInt(imgObj.style.left) - 100 + 'px';
+    console.log($('#ball').position())
 }
+right()
+
+function lose() {
+    let pic = $('#ball').position();
+    console.log(pic)
+    if(pic === pic.left + 0) {
+        alert('lose')
+    }
+}
+
+lose()
